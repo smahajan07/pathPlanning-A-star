615e50543e8bf84db8c81c1f29db8d430b39a16a
smahajan07 <mahajan.sarthak28@gmail.com>
2018-08-30 21:25:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# A-star algorithm for 2D path Planning
+
+## Standard install via command-line
+```
+git clone --recursive https://github.com/smahajan07/pathPlanning-A-star
+cd pathPlanning-A-star
+mkdir build
+cd build
+cmake ..
+make
+Run tests: ./test/cpp-test
+Run program: ./app/shell-app
+```
+## Sample Output
+
+## Performance
+
+In comparison to the pathPlanning BFS repository, this works tremendously fast. In a grid of 250px by 150 px to search for a path from (1,1) to (249,149) the total time goes down from 30sec to 4sec. Further improvements will be done. 
---
+++ app/main.cpp
@@ -0,0 +1,441 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+// Global Variables
+#define MAX 37500
+#define ROOT 0
+
+class Node{
+
+    public:
+
+        int idNum;
+        int parentNum;
+        double cost;
+        double gCost;
+        double tCost;
+        tuple<int, int> currNodePos;
+        tuple<int, int> prevNodePos;
+        void callActions(const int& idx);
+
+        void addNode(Node& newNode,const int& x, const int& y, int& id,int pNum,const int& dFlag);
+
+        void actionMoveEast(const int& idx,int x,int y, const int& x_old);
+        void actionMoveNorth(const int& idx,int x,int y, const int& y_old);
+        void actionMoveNorthEast(const int& idx,int x,int y, const int& x_old, const int& y_old);
+
+        void actionMoveSouthEast(const int& idx,int x,int y, const int& x_old, const int& y_old);
+        void actionMoveSouth(const int& idx,int x,int y, const int& y_old);
+        void actionMoveSouthWest(const int& idx,int x,int y, const int& x_old, const int& y_old);
+        void actionMoveWest(const int& idx,int x,int y, const int& x_old);
+        void actionMoveNorthWest(const int& idx,int x,int y, const int& x_old, const int& y_old);
+
+        bool checkDuplicate(tuple<int, int> nodePos,const int& idx);
+};
+
+struct compareCost{
+    bool operator()(Node &obj1,Node &obj2){
+        return obj2.cost < obj1.cost;
+    }
+};
+
+int totalNodesCreated=0;
+//queue<int> levelQ;
+priority_queue<Node, vector<Node>,compareCost> levelQ;
+queue<int> closedQ;
+
+tuple<int, int> goalNode(205,40); // Change value for Goal Node
+tuple<int, int> finalPath[400];
+int pathNodes=0;
+
+// put this in the Node file
+
+void generateNodes();
+bool checkObstacle(tuple<int, int> nodePos);
+void createSpace();
+double heuristicFn(const int& x, const int& y);
+
+
+
+Node nodesInfo[MAX];     // Array of objects of the class Node
+
+
+void generateNodes(){
+    while(!levelQ.empty()){ //&& totalNodesCreated<MAX
+        int idx = levelQ.top().idNum;
+        if(nodesInfo[idx-1].currNodePos != goalNode){
+            nodesInfo[idx-1].callActions(idx);
+        }
+        else
+          break;
+    levelQ.pop();
+    }
+}
+
+void printNode(tuple<int, int> nodePos){
+
+    int x=get<0>(nodePos);
+    int y=get<1>(nodePos);
+    if(x and y){
+      get<0>(finalPath[pathNodes])=x;
+      get<1>(finalPath[pathNodes])=y;
+//      cout<<"\nX : "<<x<<"\t"<<"Y : "<<y<<endl;
+      pathNodes++;
+    }
+}
+
+// members of class Node
+
+void Node::callActions(const int& idx){       // idx - parent , so now we will make the child nodes from this parent
+    int x = get<0>(nodesInfo[idx-1].currNodePos);
+    int y = get<1>(nodesInfo[idx-1].currNodePos);
+
+    int x_old = get<0>(nodesInfo[idx-1].prevNodePos);
+    int y_old = get<1>(nodesInfo[idx-1].prevNodePos);
+
+    actionMoveEast(idx, x, y, x_old);
+    actionMoveNorth(idx, x, y, y_old);
+    actionMoveNorthEast(idx, x, y, x_old, y_old);
+
+    actionMoveSouthEast(idx, x, y, x_old, y_old);
+    actionMoveSouth(idx, x, y, y_old);
+
+    actionMoveSouthWest(idx, x, y, x_old, y_old);
+    actionMoveWest(idx, x, y, x_old);
+    actionMoveNorthWest(idx, x, y, x_old, y_old);
+
+    closedQ.push(idx);
+}
+
+void Node::actionMoveNorth(const int& idx,int x, int y, const int& y_old){
+
+    y++;
+
+    if(y<150 && y!=y_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,0);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x,y-1);
+
+        }
+    }
+
+}
+
+void Node::actionMoveNorthEast(const int& idx,int x, int y, const int& x_old, const int& y_old){
+
+    y++;x++;
+
+    if(y<150 && x<250 && y!=y_old && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,1);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x-1,y-1);
+
+        }
+    }
+
+}
+
+void Node::actionMoveEast(const int& idx,int x, int y, const int& x_old){
+
+    x++;
+
+    if(x<250 && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,0);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x-1,y);
+
+        }
+    }
+
+}
+
+void Node::actionMoveSouthEast(const int& idx,int x, int y, const int& x_old, const int& y_old){
+
+    y--;x++;
+
+    if(y>0 && x<250 && y!=y_old && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,1);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x-1,y+1);
+
+        }
+    }
+
+}
+
+void Node::actionMoveSouth(const int& idx,int x, int y, const int& y_old){
+
+    y--;
+
+    if(y>0 && y!=y_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,0);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x,y+1);
+
+        }
+    }
+
+}
+
+void Node::actionMoveSouthWest(const int& idx,int x, int y, const int& x_old, const int& y_old){
+
+    y--;x--;
+
+    if(y>0 && x>0 && y!=y_old && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,1);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x+1,y+1);
+
+        }
+    }
+
+}
+
+void Node::actionMoveWest(const int& idx,int x, int y, const int& x_old){
+
+    x--;
+
+    if(x>0 && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,0);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x+1,y);
+
+        }
+    }
+
+}
+
+void Node::actionMoveNorthWest(const int& idx,int x, int y, const int& x_old, const int& y_old){
+
+    y++;x--;
+
+    if(y<150 && x>0 && y!=y_old && x!=x_old){
+        bool check1 = checkDuplicate(make_tuple(x,y),idx);
+        bool check2 = checkObstacle(make_tuple(x,y));
+        if(check1 && check2){
+            totalNodesCreated++;
+            nodesInfo[totalNodesCreated-1].addNode(nodesInfo[totalNodesCreated-1], x, y, totalNodesCreated, idx,1);
+            nodesInfo[totalNodesCreated-1].prevNodePos = make_tuple(x+1,y-1);
+
+        }
+    }
+
+}
+
+void Node::addNode(Node& newNode,const int& x, const int& y, int& id, int pNum,const int& dFlag){
+    newNode.currNodePos = make_tuple(x,y);
+    newNode.idNum = id;
+    newNode.parentNum = pNum;
+    dFlag?newNode.tCost = 1.4:newNode.tCost = 1;
+
+    newNode.gCost = nodesInfo[pNum-1].gCost + newNode.tCost;
+
+    newNode.cost = newNode.gCost + heuristicFn(x,y);
+    levelQ.push(newNode);
+}
+
+double heuristicFn(const int& x, const int& y){
+
+  int goalX = get<0>(goalNode);
+  int goalY = get<1>(goalNode);
+
+  return sqrt(pow((goalX-x),2) + pow((goalY-y),2));
+
+}
+
+bool Node::checkDuplicate(tuple<int, int> nodePos,const int& idx){
+    for(int i=0;i<totalNodesCreated;++i){
+        if(nodePos == nodesInfo[i].currNodePos){
+          int p = nodesInfo[i].parentNum;
+          if(nodesInfo[idx-1].cost < nodesInfo[p-1].cost)
+            nodesInfo[i].parentNum = idx;
+          return false;
+        }
+    }
+    return true;
+}
+
+bool checkObstacle(tuple<int, int> nodePos){
+    int x=get<0>(nodePos);
+    int y=get<1>(nodePos);
+
+    if(x>=55 && y>=37 && x<=105 && y<=83){
+         return false;
+    }
+
+    else if(pow((x-180),2) + pow((y-30),2)- pow(15,2) <= 0){
+        return false;
+    }
+
+    else if(((y+0.75*x-185.6) >= 0) && ((y-1.64*x +101.8) <= 0) && (y-136 <=0) && ((y+1.85*x-446.8) <= 0) && ((y-1.65*x + 211.6) >= 0) )  { // ((y-0.1*x - 82.36) <= 0) && ((y+5.42*x - 956.71) > 0) && ((y+1.85*x-446.8) >= 0) && ((y-1.65*x + 211.6) >= 0) && ((y+3.75*x - 679.75) <= 0)
+      if(((y+0.75*x-185.6) >= 0) && ((y-0.1*x -82.36) < 0) && (y+5.42*x - 956.7 <0)){
+        return true;
+      }
+      else
+        return false;
+    }
+    else
+      return true;
+}
+
+bool checkPathNode(tuple<int, int> pos){
+
+    for(int i=0;i<pathNodes;++i){
+        if((get<0>(pos) == get<0>(finalPath[i])) && (get<1>(pos) == get<1>(finalPath[i])))
+            return true;
+    }
+
+    return false;
+}
+
+void findPathAstar(tuple<int, int> nodePos){
+
+    int flag=1;
+    for(int i=totalNodesCreated;i>0;--i){
+        if(nodePos==nodesInfo[i].currNodePos){
+            flag=0;
+            int p=nodesInfo[i].parentNum;
+            printNode(nodesInfo[--p].currNodePos);
+            ++p;
+            for(int j=nodesInfo[i].cost;j>0;--j){
+              printNode(nodesInfo[--p].currNodePos);
+              p=nodesInfo[p].parentNum;
+            }
+            break;
+        }
+    }
+
+    if(flag)
+        cout<<"\nNode not found!\n";
+}
+
+void createSpace(){
+    FILE *f = fopen("../output/out.ppm", "wb");
+    int height=150,width=250,red_value=0,green_value=0,blue_value=255;
+    fprintf(f, "P6\n%i %i 255\n", width, height);
+    for (int y=0; y<height; ++y) {
+        for (int x=0; x<width; ++x) {
+
+            bool checkPath = checkPathNode(make_tuple(x,y));
+
+            if(checkPath){
+
+                fputc(120, f);
+                fputc(255, f);
+                fputc(0, f);
+            }
+
+            else{
+
+                if(x>=55 && y>=37 && x<=105 && y<=83){
+                    fputc(255, f);
+                    fputc(0, f);
+                    fputc(0, f);
+                }
+
+                else if(pow((x-180),2) + pow((y-30),2)- pow(15,2) <= 0){
+                    fputc(255, f);
+                    fputc(0, f);
+                    fputc(0, f);
+                }
+
+                else if(((y+0.75*x-185.6) >= 0) && ((y-1.64*x +101.8) <= 0) && (y-136 <=0) && ((y+1.85*x-446.8) <= 0) && ((y-1.65*x + 211.6) >= 0) ){ // ((y-0.1*x - 82.36) <= 0) && ((y+5.42*x - 956.71) > 0) && ((y+1.85*x-446.8) >= 0) && ((y-1.65*x + 211.6) >= 0) && ((y+3.75*x - 679.75) <= 0)
+
+                    if(((y+0.75*x-185.6) >= 0) && ((y-0.1*x -82.36) <= 0) && (y+5.42*x - 956.7 <=0)){
+                      fputc(red_value, f);
+                      fputc(green_value, f);
+                      fputc(blue_value, f);
+                    }
+
+                    else{
+                      fputc(255, f);
+                      fputc(0, f);
+                      fputc(0, f);
+                    }
+
+                }
+
+                else{
+                    fputc(red_value, f);
+                    fputc(green_value, f);
+                    fputc(blue_value, f);
+                }
+
+            }
+        }
+
+    }
+    fclose(f);
+}
+
+//Main function
+
+int main()
+{
+
+
+    // Enter the start node
+    int startX = 10;
+    int startY = 10;
+
+    int beginFlag = checkObstacle(goalNode);
+
+    if(beginFlag){
+
+        // Initializing start node
+        tuple<int, int> startNode(startX,startY);
+        totalNodesCreated++;
+
+        int rootID = 1;
+        nodesInfo[ROOT].addNode(nodesInfo[ROOT],startX, startY, rootID, 0,0);
+        tuple<int, int> prevNodePosRoot(-1,-1);
+        nodesInfo[ROOT].prevNodePos = prevNodePosRoot;
+        nodesInfo[ROOT].cost = 0;
+        nodesInfo[ROOT].gCost = 0;
+        levelQ.push(nodesInfo[ROOT]);
+
+
+        generateNodes();
+
+        cout<<totalNodesCreated<<endl;
+
+        // Goal node is set as a global variable, please set the global variable "goalNode"...
+                                                                     //...to see the path accordingly
+        printNode(goalNode);
+        findPathAstar(goalNode);
+
+
+        // For visual representation, optionally you can print nodes which form the final path
+        createSpace();
+    }
+    else
+        cout<<"\nPlease change Goal Node location!\n";
+
+    return 0;
+}
